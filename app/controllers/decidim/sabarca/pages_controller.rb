797e27a6c0a1a74946de7538d733eef77e035f67
Isaac Massot <isaac.mg@coditramuntana.com>
2017-09-27 18:08:23 +0200

    [WIP] feat structure participatory process, working on city close up index

--- app/controllers/decidim/sabarca/pages_controller.rb
+++ app/controllers/decidim/sabarca/pages_controller.rb
@@ -8,11 +8,22 @@ module Decidim
     # Note that it inherits from `Decidim::Features::BaseController`, which
     # override its layout and provide all kinds of useful methods.
     class PagesController < Decidim::PagesController
+      helper_method :participatory_processes_no_district, :organization_scopes
+
       def transparency
       end
       def city_close_up_index
       end
       def city_close_up_show
+      end
+
+      def participatory_processes_no_district
+        @partcipatory_processes ||=
+          Decidim::ParticipatoryProcess.where(decidim_organization_id: current_organization.id).where(decidim_scope_id: nil)
+      end
+
+      def organization_scopes
+        @scopes ||= Decidim::Scope.where(decidim_organization_id: current_organization.id)
       end
     end
   end
