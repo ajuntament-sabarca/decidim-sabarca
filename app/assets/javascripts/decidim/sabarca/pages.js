6bbb45d0e7cc5d9108ec6bdfdea58cb2b616bd34
Isaac Massot <isaac.mg@coditramuntana.com>
2017-10-02 13:15:02 +0200

    [FIX] assets compilation

--- app/assets/javascripts/decidim/sabarca/pages.js
+++ app/assets/javascripts/decidim/sabarca/pages.js
@@ -1,9 +1,9 @@
-// = require leaflet
-// = require leaflet-tilelayer-here
-// = require leaflet-svg-icon
-// = require leaflet.markercluster
-// = require jquery-tmpl
-// = require_self
+//= require leaflet
+//= require leaflet-tilelayer-here
+//= require leaflet-svg-icon
+//= require leaflet.markercluster
+//= require jquery-tmpl
+//= require_self
 
 var mapId = 'map';
 var $map = $(`#${mapId}`);
@@ -20,8 +20,8 @@ L.tileLayer.here({
 }).addTo(map);
 
 function onclick(e) {
-  current_url = window.location.href
-  detail_url = "/"+ e.target.feature.properties.id_scope
+  var current_url = window.location.href
+  var detail_url = "/"+ e.target.feature.properties.id_scope
 
   window.location.replace(current_url + detail_url)
 }
--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -1,1 +1,2 @@
-Rails.application.config.assets.precompile += %w( decidim/sabarca/* )
+Rails.application.config.assets.precompile += %w( decidim/sabarca/pages.js )
+Rails.application.config.assets.precompile += %w( decidim/sabarca/scopes-geojson.js )
